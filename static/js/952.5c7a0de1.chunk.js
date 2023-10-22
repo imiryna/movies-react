8399e8d1bfeb80a6f74651e16ec2adc61845ff16
imiryna <127667411+imiryna@users.noreply.github.com>
2023-10-22 21:23:07 +0000

    Deploying to gh-pages from @ imiryna/goit-react-hw-05-movies@a1624d810ca7c28059c77bd180cb035ae9bcfc64 🚀

---
+++ static/js/952.5c7a0de1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[952],{3952:function(e,t,i){i.r(t);i(2791);var n=i(1087),o=i(184);t.default=function(){return(0,o.jsxs)("div",{children:[(0,o.jsx)("h1",{children:"404 - Not Found!"}),(0,o.jsx)(n.rU,{to:"/",children:"Go Home"})]})}}}]);
+//# sourceMappingURL=952.5c7a0de1.chunk.js.map
