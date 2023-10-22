67a9d3ddaeee86dc323a43dacedccedcd4fe6d8d
Iryna Burlaka <i.am.blinova@gmail.com>
2023-10-22 15:22:30 +0200

    WIP

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,43 +1,39 @@
-import {useState, useEffect} from 'react';
-import {fetchTrendingMovies} from '../services/api';
+import { useState, useEffect } from 'react';
+import { fetchTrendingMovies } from '../services/api';
 import { toast } from 'react-toastify';
-import {MoviesList} from '../MoviesList/moviesList'
+import { MoviesList } from '../MoviesList/moviesList';
 import { Loader } from 'components/Loader/loader';
 import { ContainerCss } from './pageStaled/page';
 
 export const Home = () => {
- const [movies, setMovies] = useState([]);
- const [error, setError] = useState(null);
- const [isloader, setIsloader] = useState(false);
- 
- useEffect( ()=>{
-     const handleFetchMovies = async() =>{
-        try {
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [isloader, setIsloader] = useState(false);
 
-         setIsloader(true);
-         const {results} = await fetchTrendingMovies(); 
-        
-         setMovies(results);
+  useEffect(() => {
+    const handleFetchMovies = async () => {
+      try {
+        setIsloader(true);
+        const { results } = await fetchTrendingMovies();
 
-         
-        } catch (error) {
-            setError(error.message)
-            toast.error(error.message);
-        } finally {
-            setIsloader(false);
-        }
-     }
-     handleFetchMovies( )
- },[])
+        setMovies(results);
+      } catch (error) {
+        setError(error.message);
+        toast.error(error.message);
+      } finally {
+        setIsloader(false);
+      }
+    };
+    handleFetchMovies();
+  }, []);
 
- return(
+  return (
     <>
-   <ContainerCss>
-    {error && <duv>Oh no! Something does wron: {error}</duv>}
-    {movies.length > 0 && <MoviesList moviesList={movies}/>}
-   </ContainerCss>
-   {isloader && <Loader/>}
+      <ContainerCss>
+        {error && <duv>Oh no! Something does wron: {error}</duv>}
+        {movies.length > 0 && <MoviesList moviesList={movies} />}
+      </ContainerCss>
+      {isloader && <Loader />}
     </>
- )
-
-}
+  );
+};
